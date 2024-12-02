73bcdd85edc9dbc2020a5540d0437ca494d5c174
Anton Mulder <anton.mulder@wearespindle.com>
2024-12-02 13:07:21 +0100

    Refactor day 1

--- day_1/part_1.py
+++ day_1/part_1.py
@@ -1,14 +1,29 @@
-input_data = open("example.txt").read()
+def solve(input_data):
+    left = []
+    right = []
 
-left = []
-right = []
+    for line in input_data.splitlines():
+        x, y = map(int, line.split())
+        left.append(x)
+        right.append(y)
 
-for line in input_data.splitlines():
-    x, y = map(int, line.split())
-    left.append(x)
-    right.append(y)
+    left.sort()
+    right.sort()
 
-left.sort()
-right.sort()
+    return sum(abs(x - y) for x, y in zip(left, right, strict=False))
 
-print(sum(abs(x - y) for x, y in zip(left, right, strict=False)))
+
+if __name__ == "__main__":
+    input_data = open("input.txt").read()
+    print(solve(input_data))
+
+EXAMPLE = """3   4
+4   3
+2   5
+1   3
+3   9
+3   3"""
+
+
+def test_solve():
+    assert solve(EXAMPLE) == 11
--- day_1/part_2.py
+++ day_1/part_2.py
@@ -1,13 +1,29 @@
 from collections import defaultdict
 
-input_data = open("example.txt").read()
 
-left = []
-right = defaultdict(lambda: 0)
+def solve(input_data):
+    left = []
+    right = defaultdict(lambda: 0)
 
-for line in input_data.splitlines():
-    x, y = map(int, line.split())
-    left.append(x)
-    right[y] += 1
+    for line in input_data.splitlines():
+        x, y = map(int, line.split())
+        left.append(x)
+        right[y] += 1
 
-print(sum(x * right[x] for x in left))
+    return sum(x * right[x] for x in left)
+
+
+if __name__ == "__main__":
+    input_data = open("input.txt").read()
+    print(solve(input_data))
+
+EXAMPLE = """3   4
+4   3
+2   5
+1   3
+3   9
+3   3"""
+
+
+def test_solve():
+    assert solve(EXAMPLE) == 31
