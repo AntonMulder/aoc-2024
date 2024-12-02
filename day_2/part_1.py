e93f8c47db11c7201164c39b6f90345ada7e95b2
Anton Mulder <anton.mulder@wearespindle.com>
2024-12-02 09:27:36 +0100

    Refactor Day 2

--- day_2/part_1.py
+++ day_2/part_1.py
@@ -1,21 +1,35 @@
-input_data = open("example.txt").read()
-
-valid = 0
-
-
 def is_valid(level):
     if level == sorted(level) or level == sorted(level, reverse=True):
-        diff = [abs(y - x) for x, y in zip(level, level[1:])]
+        diff = (abs(y - x) for x, y in zip(level, level[1:]))
 
         if all(1 <= x <= 3 for x in diff):
             return True
     return False
 
 
-for line in input_data.splitlines():
-    level = list(map(int, line.split()))
+def solve(input_data):
+    valid = 0
+    for line in input_data.splitlines():
+        level = list(map(int, line.split()))
 
-    if is_valid(level):
-        valid += 1
+        if is_valid(level):
+            valid += 1
 
-print(valid)
+    return valid
+
+
+if __name__ == "__main__":
+    input_data = open("input.txt").read()
+    print(solve(input_data))
+
+
+EXAMPLE = """7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9"""
+
+
+def test_solve():
+    assert solve(EXAMPLE) == 2
--- day_2/part_2.py
+++ day_2/part_2.py
@@ -1,8 +1,3 @@
-input_data = open("example.txt").read()
-
-valid = 0
-
-
 def is_valid(level):
     if level == sorted(level) or level == sorted(level, reverse=True):
         diff = (abs(y - x) for x, y in zip(level, level[1:]))
@@ -12,13 +7,33 @@ def is_valid(level):
     return False
 
 
-for line in input_data.splitlines():
-    level = list(map(int, line.split()))
+def solve(input_data):
+    valid = 0
 
-    for x in range(len(level)):
-        temp_level = level[:x] + level[x + 1 :]
-        if is_valid(temp_level):
-            valid += 1
-            break
+    for line in input_data.splitlines():
+        level = list(map(int, line.split()))
 
-print(valid)
+        for x in range(len(level)):
+            temp_level = level[:x] + level[x + 1 :]
+            if is_valid(temp_level):
+                valid += 1
+                break
+
+    return valid
+
+
+if __name__ == "__main__":
+    input_data = open("input.txt").read()
+    print(solve(input_data))
+
+
+EXAMPLE = """7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9"""
+
+
+def test_solve():
+    assert solve(EXAMPLE) == 4
