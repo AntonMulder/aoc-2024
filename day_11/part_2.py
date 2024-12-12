eb572a573188b069c64e01fbe6dfb55247f327dc
Anton Mulder <anton.mulder@wearespindle.com>
2024-12-12 09:20:12 +0100

    Day 11

---
+++ day_11/part_2.py
@@ -0,0 +1,39 @@
+from collections import defaultdict
+
+
+def solve(input_data):
+    # Create list of stones.
+    stones = defaultdict(lambda: 0)
+    for stone in map(int, input_data.split(" ")):
+        stones[stone] += 1
+
+    for _ in range(75):
+        new_stones = defaultdict(lambda: 0)
+        for stone, value in stones.items():
+            if stone == 0:
+                new_stones[1] += value
+            elif len(str(stone)) % 2 == 0:
+                stone = str(stone)
+                left, right = (
+                    int(stone[: len(stone) // 2]),
+                    int(stone[len(stone) // 2 :]),
+                )
+                new_stones[left] += value
+                new_stones[right] += value
+            else:
+                new_stones[stone * 2024] += value
+        stones = new_stones
+
+    return sum(stones[x] for x in stones.keys())
+
+
+if __name__ == "__main__":
+    input_data = open("input.txt").read()
+    print(solve(input_data))
+
+
+EXAMPLE = "125 17"
+
+
+def test_solve():
+    assert solve(EXAMPLE) == 65601038650482
